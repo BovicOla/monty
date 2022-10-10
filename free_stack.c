5b06bc4401cd6c0f3fbd18a0945369ff0e90249e
root <Olaleyebode001@gmail.com>
2022-10-09 18:04:14 -0700

    Added files

--- free_stack.c
+++ free_stack.c
@@ -1,40 +1,17 @@
 #include "monty.h"
 /**
- * f_div - divides the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
+* free_stack - frees a doubly linked list
+* @head: head of the stack
 */
-void f_div(stack_t **head, unsigned int counter)
+void free_stack(stack_t *head)
 {
-	stack_t *h;
-	int len = 0, aux;
+	stack_t *aux;
 
-	h = *head;
-	while (h)
+	aux = head;
+	while (head)
 	{
-		h = h->next;
-		len++;
+		aux = head->next;
+		free(head);
+		head = aux;
 	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	if (h->n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	aux = h->next->n / h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
 }
